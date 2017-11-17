19b13b58d68bd451fcd39e850e651e00407191e4
Denis BOURGE <bourge.denis@gmail.com>
2017-11-17 13:44:21 +0100

    autodoc update

--- docs/autodoc.py
+++ docs/autodoc.py
@@ -1,4 +1,22 @@
 """Autodoc."""
 
+import subprocess
 import os
-os.system("sphinx-apidoc -f -o . ../docknv")
+import shutil
+
+# Update docknv modules .rst
+print("Updating modules...")
+subprocess.call(["sphinx-apidoc", "-f", "-o", ".", "../docknv"])
+
+# Clean build
+build_path = os.path.join("_build", "html")
+if os.path.exists(build_path):
+    print("Removing existing HTML build...")
+    shutil.rmtree(build_path)
+
+# Build
+print("Building HTML docs...")
+if os.name == "nt":
+    subprocess.call([".\make.bat", "html"])
+else:
+    subprocess.call(["make", "html"])
